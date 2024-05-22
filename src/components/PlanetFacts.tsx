0029c70f97b2a57ca1015aea21b714c174514fbb
PioKl <kosiu342@gmail.com>
2024-05-22 16:47:20 +0200

    Functionality Update (dynamic data update for all planets)

--- src/components/PlanetFacts.tsx
+++ src/components/PlanetFacts.tsx
@@ -1,44 +1,110 @@
+import { useState } from "react";
 import "../styles/PlanetFacts.scss";
 import { Planet } from "../types/types";
 
 interface PlanetFactsProps {
-  planet: Planet[];
+  planets: Planet[];
+  planetIndex: number;
 }
 
-const PlanetFacts: React.FC<PlanetFactsProps> = ({ planet }) => {
-  console.log(planet[0].images.planet);
+const PlanetFacts: React.FC<PlanetFactsProps> = ({ planets, planetIndex }) => {
+  console.log(planetIndex);
+  const [informationOptionChoosed, setInformationOptionChoosed] =
+    useState("overview");
+  const handleButtonClick = (name: string) => {
+    setInformationOptionChoosed(name);
+  };
   return (
     <div className="wrapper planetContainer">
       <div className="planetPictureContainer">
         <img
-          className="planetPictureContainer__image"
-          src={planet[0].images.planet}
+          className={`planetPictureContainer__image --${planets[
+            planetIndex
+          ].name.toLowerCase()}`}
+          src={
+            informationOptionChoosed === "overview" ||
+            informationOptionChoosed === "geology"
+              ? planets[planetIndex].images.planet
+              : "" || informationOptionChoosed === "structure"
+              ? planets[planetIndex].images.internal
+              : ""
+          }
           alt=""
         />
+        {informationOptionChoosed === "geology" && (
+          <img
+            className={`planetPictureContainer__geologyImage --${planets[
+              planetIndex
+            ].name.toLowerCase()}`}
+            src={planets[planetIndex].images.geology}
+            alt=""
+          />
+        )}
       </div>
       <div className="planetButtonsContainer">
-        <button className="planetButtonsContainer__button --one --active">
+        <button
+          className={`planetButtonsContainer__button --one ${
+            informationOptionChoosed === "overview"
+              ? `--active --${planets[planetIndex].name.toLowerCase()}`
+              : ""
+          }`}
+          onClick={() => handleButtonClick("overview")}
+        >
           Overview
-          <span></span>
+          <span
+            className={`planetButtonsContainer__span --${planets[
+              planetIndex
+            ].name.toLowerCase()}`}
+          ></span>
         </button>
-        <button className="planetButtonsContainer__button --two">
-          <span></span>
+        <button
+          className={`planetButtonsContainer__button --two ${
+            informationOptionChoosed === "structure"
+              ? `--active --${planets[planetIndex].name.toLowerCase()}`
+              : ""
+          }`}
+          onClick={() => handleButtonClick("structure")}
+        >
+          <span
+            className={`planetButtonsContainer__span --${planets[
+              planetIndex
+            ].name.toLowerCase()}`}
+          ></span>
         </button>
-        <button className="planetButtonsContainer__button --three">
-          <span></span>
+        <button
+          className={`planetButtonsContainer__button --three ${
+            informationOptionChoosed === "geology"
+              ? `--active --${planets[planetIndex].name.toLowerCase()}`
+              : ""
+          }`}
+          onClick={() => handleButtonClick("geology")}
+        >
+          <span
+            className={`planetButtonsContainer__span --${planets[
+              planetIndex
+            ].name.toLowerCase()}`}
+          ></span>
         </button>
       </div>
       <div className="planetInfoContainer">
-        <h1 className="planetInfoContainer__heading">Mercury</h1>
+        <h1 className="planetInfoContainer__heading">
+          {planets[planetIndex].name}
+        </h1>
         <p className="planetInfoContainer__info">
-          Mercury is the smallest planet in the Solar System and the closest to
-          the Sun. Its orbit around the Sun takes 87.97 Earth days, the shortest
-          of all the Sun's planets. Mercury is one of four terrestrial planets
-          in the Solar System, and is a rocky body like Earth.
+          {informationOptionChoosed === "overview" &&
+            planets[planetIndex].overview.content}
+          {informationOptionChoosed === "structure" &&
+            planets[planetIndex].structure.content}
+          {informationOptionChoosed === "geology" &&
+            planets[planetIndex].geology.content}
         </p>
         <div className="planetInfoContainer__source">
           <span>Source</span>
-          <a className="planetInfoContainer__link" href="">
+          <a
+            className="planetInfoContainer__link"
+            href={planets[planetIndex].overview.source}
+            target="_blank"
+          >
             Wikipedia
           </a>
         </div>
@@ -46,19 +112,27 @@ const PlanetFacts: React.FC<PlanetFactsProps> = ({ planet }) => {
       <div className="planetQuickInfoContainer">
         <div className="planetCharacteristics">
           <span className="planetCharacteristics__first">Rotation Time</span>
-          <span className="planetCharacteristics__second">58.6 Days</span>
+          <span className="planetCharacteristics__second">
+            {planets[planetIndex].rotation}
+          </span>
         </div>
         <div className="planetCharacteristics">
           <span className="planetCharacteristics__first">Revolution Time</span>
-          <span className="planetCharacteristics__second">87.97 Days</span>
+          <span className="planetCharacteristics__second">
+            {planets[planetIndex].revolution}
+          </span>
         </div>
         <div className="planetCharacteristics">
           <span className="planetCharacteristics__first">Radius</span>
-          <span className="planetCharacteristics__second">2,439.7 Km</span>
+          <span className="planetCharacteristics__second">
+            {planets[planetIndex].radius}
+          </span>
         </div>
         <div className="planetCharacteristics">
           <span className="planetCharacteristics__first">Average Temp.</span>
-          <span className="planetCharacteristics__second">430°c</span>
+          <span className="planetCharacteristics__second">
+            {planets[planetIndex].temperature}
+          </span>
         </div>
       </div>
     </div>
